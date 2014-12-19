8e3d6244ad751bb298da77584e93314bb471541a
Marie Bolger <m.bolger@fz-juelich.de>
2014-12-19 14:47:14 +0100

    a few improvements

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,10 +5,11 @@
 
   console.log('Hello, Gene ID to MapMan bin!');
 
-  var appContext = $('[data-app-name="test-mapman"]');
+  var appContext = $('[data-app-name="mapman"]');
 
 var templates = {
-  resultTable: _.template('<table class="table"><thead><tr><th>AGI code</th><th>MapMan bin code</th><th>MapMan bin description</th></tr></thead><tbody><% _.each(genes, function(r) { %><tr><td><%= r.identifier %></td><td><%= r.bincode %></td><td><%= r.binname %></td></tr><% }) %></tbody></table>')
+  resultTable: _.template('<table class="table"><thead><tr><th>AGI code</th><th>MapMan bin code</th><th>MapMan bin description</th></tr></thead><tbody><% _.each(genes, function(r) { %><tr><td><%= r.identifier %></td><td><%= r.bincode %></td><td><%= r.binname %></td></tr><% }) %></tbody></table>'),
+  failTable: _.template('<ul><% _.each(failedgenes, function(r) { %><li><%= r.identifier %></li><% }) %></ul>')
 };
 
   var form = $('form[name=mapman-query]', appContext);
@@ -38,17 +39,13 @@ var templates = {
       hasError = true;
     }
 
-   console.log(texts);   
+   //console.log(texts);   
 
    var query ={
      locus:texts
-     //locus1:'AT2G&9730',
-     //locus2:'AT2G39731',
-     //locus3:'AT2G39732',
-     //locus4:'AT2G39733'
    };  
     if (! hasError) {
-      $('.results').html('<pre><code>' + JSON.stringify(query, null, 2) + '</code></pre>');
+      //$('.results').html('<pre><code>' + JSON.stringify(query, null, 2) + '</code></pre>');
 
       Agave.api.adama.getStatus({}, function(resp) {
         if (resp.obj.status === 'success') {
@@ -64,15 +61,23 @@ var templates = {
     }
 
     var showResults = function( json ) {
-      console.log(JSON.stringify(json, null, 2));
+      //console.log(JSON.stringify(json, null, 2));
       // show error message for invalid object
       if ( ! ( json && json.obj ) ) {
         $( '.results', appContext ).html( '<div class="alert alert-danger">Invalid response!</div>' );
        return;
       }
+      
 
-      $( '.results', appContext ).html( templates.resultTable( json.obj.result ) );
-      $( '.results table', appContext ).dataTable();
+      if (json.obj.result.failedgenes){
+        //console.log('failed genes');
+        $('.messages', appContext).append('<div class="alert alert-danger">One or more entries did not return a result - please verfiy that they are valid AGI codes'+templates.failTable(json.obj.result)+'</div>');
+      }
+
+      if (json.obj.result.genes.length > 0){
+        $( '.results', appContext ).html( templates.resultTable( json.obj.result ) );
+        $( '.results table', appContext ).dataTable();
+      }
     };
 
   });
